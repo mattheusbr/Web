16c4dd13debd3b285e1643635d7432748fcc51c0
Matheus Bruno <matheusferreirabruno@hotmail.com>
2022-04-19 13:55:40 -0300

    Adicionando suporte a Vue JS

--- Trabalho carterinha estudante/script.js
+++ Trabalho carterinha estudante/script.js
@@ -1,33 +1,20 @@
-window.oninput = function(event){
-       
-    switch (event.target.id) {
-        case "nome":
-            PreencherTexto("#d-nome", "#nome")
-            break;
-        case "faculdade":
-            PreencherTexto("#d-faculdade", "#faculdade")
-            break;
-        case "curso":
-            PreencherTexto("#d-curso", "#curso")
-        break;
-        case "cpf":
-            PreencherTexto("#d-cpf", "#cpf")
-            break;
-        case "rg":
-            PreencherTexto("#d-rg", "#rg")
-        break;
-        case "dataNasc":
-            PreencherTexto("#d-nascimento", "#dataNasc")
-            break;
+const meuPrimeiroApp = Vue.createApp({
+    data (){
+        return {            
+            nome: '',
+            faculdade: '',
+            curso: '',
+            cpf: '',
+            rg: '',
+            dataNascimento: '',
+            genero: ''
+        }        
+    },
+    methods: {
+        LimparCampos() {
+           document.location.reload(true);
+        }   
     }   
- };
+})
 
- function PreencherTexto(campoDireita, campoEsquerda) {
-    document.querySelector(campoDireita).innerText = document.querySelector(campoEsquerda).value;
- };
- 
- function LimparCampos() {
-    document.location.reload(true);
- }
-
- 
+const mountedApp = meuPrimeiroApp.mount('#divPrincipal')
